50cd20ee2aabe551475c42c98043b20ba82793a6
Steven Vergenz <vergenzs@gmail.com>
2020-02-14 09:36:49 -0800

    Initial drop

---
+++ Assets/Altspace/Export Assets/MreMetadata.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Configure an MRE that binds to the scene
+/// </summary>
+[CreateAssetMenu(menuName = "MRE Configuration")]
+public class MreMetadata : ScriptableObject
+{
+    /// <summary>
+    /// A URI to the MRE server
+    /// </summary>
+    [Tooltip("A URI to the MRE server")]
+    public string Uri;
+
+    /// <summary>
+    /// Whether the MRE should track users
+    /// </summary>
+    [Tooltip("Whether the MRE should track users")]
+    public bool TrackUsers;
+}
